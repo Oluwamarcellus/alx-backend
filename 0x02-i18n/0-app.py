a2ea145967d7619ab5c87f039f913833af1d0a63
Ayowale <haydexwisdom@gmail.com>
2024-02-06 18:39:16 +0000

    Task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+0-app task
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+@app.route("/")
+def index() -> str:
+    """
+    basic Flask app that simply outputs “Welcome to Holberton” as
+    page title (<title>) and “Hello world” as header (<h1>)
+    """
+    return render_template("0-index.html")
+
+if __name__ == "__main__":
+    app.run()
